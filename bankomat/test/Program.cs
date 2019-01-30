0eb07cf34b1c5d8e6ae6ba88da7bc261e39579c4
LAxundzade <44810128+LAxundzade@users.noreply.github.com>
2019-01-30 14:27:15 +0400

    Add files via upload

---
+++ bankomat/test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miqdar = 0;
+            int[] count = new int[6];
+            count[0] = 100;
+            count[1] = 50;
+            count[2] = 20;
+            count[3] = 10;
+            count[4] = 5;
+            count[5] = 1;
+            Console.Write("Bankomatdan cixaracaginiz miqdari daxil edin:");
+            int money = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count.Length; i++)
+            {
+                miqdar = money / count[i];
+                Console.Write(miqdar+" eded " + count[i]+", ");
+                money = money % count[i];
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
